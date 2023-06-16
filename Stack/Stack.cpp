a06cc74ef31c2e6dc01f079dc663a4770f0f910b
dzulfaqaar <fikri6aul@gmail.com>
2023-06-16 10:52:29 +0700

    int push

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -13,5 +13,24 @@ public:
 	StackArray() {
 		top = -1;
 	}
-	 
+	int push(int element) {
+		if (top == 4) {//step 1
+			cout << "Number of data exceeds the limit.  " << endl;
+			return 0;
+		}
+		top++;
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << "ditambhakan(pushed)" << endl;
+
+		return element;
+	}
+
+	void pop() {
+		if (empty()) {//step 1
+			cout << "stack is empty. cannot pop." << endl;
+
+		}
+	}
+};
 	
